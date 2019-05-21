b9d5ad7ef879349dab285809c481aa5df8206c6b
alphonso <lingrui_xiong@163.com>
2019-05-21 18:46:43 +0800

    company info update

--- src/main/java/com/booldata/serviceota/model/Company.java
+++ src/main/java/com/booldata/serviceota/model/Company.java
@@ -10,52 +10,63 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.NotBlank;
+
 //企业信息
 @Entity
 @Table(name = "td_companys")
-public class Company  extends AbstractEntity implements Serializable {
+public class Company extends AbstractEntity implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6723105361216910396L;
-	
-	@Id
-	@GeneratedValue(generator = "system-uuid")
-	@GenericGenerator(name = "system-uuid", strategy = "uuid")
-	@Column(nullable=false,length=32, unique = true)  
-    private String id;  
-	
-	@NotBlank(message = "企业名称不能为空")
-	@Column(nullable = false, length = 100)
-	private String name;
-	
-	private String logo;// 企业logo
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6723105361216910396L;
 
-	public String getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(nullable = false, length = 32, unique = true)
+    private String id;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    @NotBlank(message = "企业名称不能为空")
+    @Column(nullable = false, length = 100)
+    private String name;
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    //by ado
+    @NotBlank(message = "企业证书不能为空")
+    @Column(nullable = false, length = 100)
+    private String certificate;
 
-	public String getLogo() {
-		return logo;
-	}
+    //法人信息
+    //负责人信息
+    //
 
-	public void setLogo(String logo) {
-		this.logo = logo;
-	}
-	
-	
+
+    private String logo;// 企业logo
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
 
 }
